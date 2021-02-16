5c71e6fd0d3e7e98e73605721346e9bbe2cee41f
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-16 18:17:36 +0100

    Create ex11.js

---
+++ ex11.js
@@ -0,0 +1,12 @@
+
+showPrimes(20);
+
+// Prime (whose factors are only 1 and itself)
+// Composite (whose factors are
+
+// 12 = 1, 2, 3, 4, 6, 12
+// Can be divided evenly by its factors and
+
+// 11 = 1, 11
+// 13 = 1, 13
+

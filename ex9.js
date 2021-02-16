a65ef49675aa77bcfa773092fd0a8842c86d69e7
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-16 15:56:56 +0100

    Update ex9.js

--- ex9.js
+++ ex9.js
@@ -1,6 +1,3 @@
-
-const marks = [80, 80, 50];
-
 // Average = 70.
 
 // 01-59: F
@@ -9,19 +6,24 @@ const marks = [80, 80, 50];
 // 80-89: B
 // 90-100: Average
 
-console.log(calculateGrade(marks));
+const marks = [80, 80, 50];
 
-function calculateGrade(marks)  {
-    let sum = 0;
-    for (let mark of marks)
-    sum =+ mark;
-    let average = sum / marks.length;
-    
+console.log(calculateGrade(marks)); {
+const average = calculateAverage(marks);
     if (average < 60) return 'F';
     if (average < 70) return 'D';
     if (average < 80) return 'C';
     if (average < 90) return 'B';
     return 'A';
+}
+
+    function calculateAverage(array)  {
+        let sum = 0;
+        for (let value of array)
+        sum =+ value;
+        return sum / array.length;
+    }
+    
 
     // more simplistic version of below 
     // if (average >= 0 && average <= 59) return 'F';
@@ -29,4 +31,3 @@ function calculateGrade(marks)  {
     // Single responsibility principal
 
 
-}

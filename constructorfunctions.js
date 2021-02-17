2b1672d065503cf632c22ba079c781a4739d6c28
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-17 18:47:34 +0100

    update

--- constructorfunctions.js
+++ constructorfunctions.js
@@ -13,3 +13,11 @@ function Circle(radius) {
 const circle = new Circle(1);
 
 // Both patterns are equally good - use Factory with Javascript
+
+let x = {};
+
+// let x = new Object();
+
+new String(); // ''. "", `` // 
+new Boolean(); // true, false - boolean literals
+new Number(); // 1, 2, 3, ...

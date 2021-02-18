dcc93dd2e0d3d1d771f08b800df9a57f3b2711ec
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-18 22:46:58 +0100

    Create templateliterals.js

---
+++ templateliterals.js
@@ -0,0 +1,31 @@
+
+// String Primitive
+const message = 
+'This is my\n; + 
+'\'first\' message';
+
+// Object literals {}
+// Boolean true, false
+// String '', ""
+// Template `` USE TEMPLATE LITERALS
+
+
+
+// cont another = 
+// `This is my
+// 'first' message`;
+
+const name = 'Kris'
+const message = 'Hi ' + name + ',\n';
+
+cont another = 
+`Hi ${name} ${2 + 3},
+
+Thank you for joining my mailing list.
+
+Regards,
+Kris';
+
+
+
+

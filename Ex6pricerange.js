0fdcce9d2f53d0ffbe568466a75cfa861757d824
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-21 10:10:13 +0100

    Create Ex6pricerange.js

---
+++ Ex6pricerange.js
@@ -0,0 +1,9 @@
+
+let priceRanges = [
+    { label: '£', tooltip: 'Inexpensive', minPerPerson: 0, maxPerPerson: 10 }
+    { label: '££', tooltip: 'Moderate', minPerPerson: 11, maxPerPerson: 20 }
+    { label: '£££', tooltip: 'Expensive', minPerPerson: 21, maxPerPerson: 10 }
+];
+let restaurants = [
+    { averagePerPerson: 5 }
+]

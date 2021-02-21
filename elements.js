6615fc849b53a10da9021fa1654cab061c89b84a
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-21 17:56:38 +0100

    elements array

---
+++ elements.js
@@ -0,0 +1,16 @@
+
+const numbers = [3, 4];
+//numbers = [];
+
+
+// End
+numbers.push(5, 6)
+
+
+// Beginning
+numbers.unshift(1, 2);
+
+// End
+numbers.splice(2, 0, 'a', 'b')
+
+console.log(numbers);

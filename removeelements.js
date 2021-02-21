e05567457a127d97bf54df65798b857fea2f970f
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-21 18:30:52 +0100

    Create removeelements.js

---
+++ removeelements.js
@@ -0,0 +1,14 @@
+
+const numbers = [1, 2, 3, 4];
+
+// End
+const last = numbers.pop();
+console.log(numbers);
+console.log(last);
+
+// Beginnng
+const first = numbers.unshift();
+
+// Middle
+numbers.splice(2, 2);
+console.log(numbers);

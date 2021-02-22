9cc59ac9840c8b76b95c821e097e01f42384e5bf
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-22 11:33:34 +0100

    Create sortingarrays.js

---
+++ sortingarrays.js
@@ -0,0 +1,28 @@
+
+// const numbers = [2, 3, 1];
+// numbers.sort();
+// console.log(numbers);
+// 
+// numbers.reverse()
+// console.log(numbers);
+
+const courses = [
+    { id: 1, name: 'Node.js' },
+    { id: 2, name: 'Javascript' },
+];
+
+courses.sort(function(a, b) {
+    // a < b => -1
+    // a > b => 1
+    // a === b => 09
+
+    const nameA = a.name.toUpperCase();
+    const nameB = b.name.toLowerCase();
+    
+    if (nameA < nameB) return -1;
+    if (nameA > nameB) return 1;
+    return 0;
+}
+
+
+console.log(courses);

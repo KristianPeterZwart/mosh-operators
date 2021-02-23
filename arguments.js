77a797aef739e74ea00e35a840603371d27aa724
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-23 18:41:15 +0100

    Create arguments.js

---
+++ arguments.js
@@ -0,0 +1,18 @@
+// Javascript is a dynamic language 
+
+// Declare variable set it to a nunmber, change its type ans set it to a string
+
+// let x = 1;
+// x = 'a';
+
+function sum() {
+    let total = 0;
+    for (let value of arguments)
+    total += value;
+    return total;
+
+    console.log(arguments);
+    return a + b; // 1 + undefined
+    }
+
+    console.log(sum(1, 2, 3, 4, 5, 10));

0c451e6436c40b521d324b610403a2764aa01e60
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-23 16:33:24 +0100

    Create ex6getmax.js

---
+++ ex6getmax.js
@@ -0,0 +1,29 @@
+
+const numbers = [1, 2, 3, 4];
+
+const max = getMax([1, 2, 3, 3, 4]);
+
+console.log(max);
+
+function getMax(array) {
+    if (array.length === 0) return udefined;
+
+    // let max = array[0];
+// 
+    // for (let i = 1; i <array.length; i++)
+    // if (array[i] > max)
+    // max = array[i];
+// 
+    // return max;
+}
+
+array.reduce((accumulator, current) => {
+if (current > accumulator) return current;
+return accummulator;
+
+return (current > accumulator) ? current : accumulator;
+});
+}
+
+// reduce method 
+// array.reduce((a, b) => (a > b) ? a : b);

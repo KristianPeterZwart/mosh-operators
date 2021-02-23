52626d69e09b36091380a6c74880d9b825c6689f
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-23 18:34:20 +0100

    Update functionexpression.js

--- functionexpression.js
+++ functionexpression.js
@@ -1,12 +1,27 @@
-// 2 ways to define a function
+// What is hoisting in Javascript?
+// Moving function declarations to the top of the file
+// done automatially by Javascript engine
 
 
-// Function Declaration
+
+
 function walk() {
     console.log('walk');
 }
 
+// 2 ways to define a function
+
+
+// Function Declaration
+walk();
+
+
+
+
+
 // Anonymous Function Expression
+run()''
+
 let run = function() {
     console.log('run');
 };

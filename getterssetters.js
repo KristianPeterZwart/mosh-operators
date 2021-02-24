c3afcfed7b8a52d2b9d60e0d277a16c742fa69d6
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-24 10:05:15 +0100

    Update getterssetters.js

--- getterssetters.js
+++ getterssetters.js
@@ -2,9 +2,19 @@
 const person = {
     firstName: 'Kristian',
     lastName: 'Zwart',
-    fullName() {
+    get fullName() {
         return `${person.firstName} ${person.lastName}`;
+    }, 
+    set fullName(value) {
+            const parts = value.split(' ');
+            this.firstName = parts[0];
+            this.lastName = parts[1];
     }
 };
 
-console.log(person.fullName());
+person.fullName = 'Nigel Kennedy';
+
+// getters => access properties
+// setters => change (mutate) them
+
+console.log(person);

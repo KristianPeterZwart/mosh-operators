3707229930db65cf5ba9bbb2f6416c9cabb7d697
Kristianpeterzwart <kristianzwartdesign@gmail.com>
2021-02-24 11:38:07 +0100

    the this keyword

---
+++ this.js
@@ -0,0 +1,26 @@
+// function part of method is called  -> object
+// regular function => global (window, global)
+
+const video = {
+    title: 'a', 
+    tags: ['a', 'b', 'c'],
+    showTags() {
+        this.tags.forEach(function(tag) {
+        console.log(this.title, tag);
+    }, this);
+}
+};
+
+video.showTags();
+
+// video.stop = function() {
+// console.log(this);
+// };
+
+// function Video(title) {
+    // this.title = title;
+    // console.log(this);
+// }
+// 
+// const v = new Video('b'); 
+// 
